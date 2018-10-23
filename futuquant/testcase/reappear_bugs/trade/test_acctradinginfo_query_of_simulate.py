ef0ba6ec0fc4de442ce6bb645a347b036ce86776
winniewang <winniewang@futunn.com>
2018-10-23 16:09:59 +0800

    模拟交易查询最大可买可卖 复现bug用例更新

--- futuquant/testcase/reappear_bugs/trade/test_acctradinginfo_query_of_simulate.py
+++ futuquant/testcase/reappear_bugs/trade/test_acctradinginfo_query_of_simulate.py
@@ -6,7 +6,9 @@ import unittest
 
 
 class AcctradinginfoQuery(unittest.TestCase):
-    # bug单：当入参order_id正确时，未按照该order_id的价格计算最大可买可卖
+    # bug单1：当入参order_id正确时，未按照该order_id的价格计算最大可买可卖
+    # bug单2：港股下单后最大可买可卖的max_cash_buy字段有时显示为0--如果执行print(order_id)不会显示0，否则显示0.偶现。
+    # bug单3：place_order指定价格和下单提交的价格不一致
     def test_acctradinginfo_query_hk_normal_order_id(self):
         # 测试点：港股普通订单
         order_type = OrderType.NORMAL
@@ -33,23 +35,8 @@ class AcctradinginfoQuery(unittest.TestCase):
             self.assertTrue(ret_data[k][0] != '')
         print(ret_data)
 
-    def test_acctradinginfo_query_hk_warrant_normal(self):
-        # 测试点：港股涡轮普通订单
-        order_type = OrderType.NORMAL
-        code = 'HK.58354'
-        price = 0.8
-        order_id = None
-        adjust_limit = random.random()
-        ret_code, ret_data = trade_ctx_hk.acctradinginfo_query(order_type, code, price, order_id,
-                                                                    adjust_limit=adjust_limit, trd_env=TrdEnv.SIMULATE,
-                                                                    acc_id=0, acc_index=0)
-        self.assertEqual(ret_code, RET_OK)
-        keys = ['max_cash_buy', 'max_cash_and_margin_buy', 'max_position_sell', 'max_sell_short', 'max_buy_back']
-        for k in keys:
-            self.assertTrue(ret_data[k][0] != '')
-        print(ret_data)
 
-    # 美股
+    # bug单：模拟交易查询美股普通订单的最大可买可卖返回失败
     def test_acctradinginfo_query_us_normal(self):
         # 测试点：美股普通订单
         order_type = OrderType.NORMAL
@@ -65,71 +52,7 @@ class AcctradinginfoQuery(unittest.TestCase):
         print(ret_code)
         print(ret_data)
 
-    def test_acctradinginfo_query_us_market(self):
-        # 测试点：美股市价单
-        order_type = OrderType.MARKET
-        code = 'US.QD'
-        price = 4.8
-        order_id = None
-        adjust_limit = random.random()
-        ret_code, ret_data = trade_ctx_us.acctradinginfo_query(order_type, code, price, order_id,
-                                                                    adjust_limit=adjust_limit, trd_env=TrdEnv.SIMULATE,
-                                                                    acc_id=0, acc_index=0)
-        self.assertEqual(ret_code, RET_ERROR)
-        print(ret_data)
-
-    def test_acctradinginfo_query_us_absolute_limit(self):
-        # 测试点：美股限价单
-        order_type = OrderType.ABSOLUTE_LIMIT
-        code = 'US.JD'
-        price = 24
-        order_id = None
-        adjust_limit = random.random()
-        ret_code, ret_data = trade_ctx_us.acctradinginfo_query(order_type, code, price, order_id,
-                                                                    adjust_limit=adjust_limit, trd_env=TrdEnv.SIMULATE,
-                                                                    acc_id=0, acc_index=0)
-        self.assertEqual(ret_code, RET_ERROR)
-        print(ret_data)
-
-    def test_acctradinginfo_query_us_auction(self):
-        # 测试点：美股竞价单
-        order_type = OrderType.AUCTION
-        code = 'US.PDD'
-        price = 21
-        order_id = None
-        adjust_limit = random.random()
-        ret_code, ret_data = trade_ctx_us.acctradinginfo_query(order_type, code, price, order_id,
-                                                                    adjust_limit=adjust_limit, trd_env=TrdEnv.SIMULATE,
-                                                                    acc_id=0, acc_index=0)
-        self.assertEqual(ret_code, RET_ERROR)
-        print(ret_data)
-
-    def test_acctradinginfo_query_us_auction_limit(self):
-        # 测试点：美股竞价限价单
-        order_type = OrderType.AUCTION_LIMIT
-        code = 'US.LFC'
-        price = 11.5
-        order_id = None
-        adjust_limit = random.random()
-        ret_code, ret_data = trade_ctx_us.acctradinginfo_query(order_type, code, price, order_id,
-                                                                    adjust_limit=adjust_limit, trd_env=TrdEnv.SIMULATE,
-                                                                    acc_id=0, acc_index=0)
-        self.assertEqual(ret_code, RET_ERROR)
-        print(ret_data)
-
-    def test_acctradinginfo_query_us_special_limit(self):
-        # 测试点：美股特别限价单
-        order_type = OrderType.SPECIAL_LIMIT
-        code = 'US.QD'
-        price = 4.8
-        order_id = None
-        adjust_limit = random.random()
-        ret_code, ret_data = trade_ctx_us.acctradinginfo_query(order_type, code, price, order_id,
-                                                                    adjust_limit=adjust_limit, trd_env=TrdEnv.SIMULATE,
-                                                                    acc_id=0, acc_index=0)
-        self.assertEqual(ret_code, RET_ERROR)
-        print(ret_data)
-
+    # bug单：模拟交易查询美股普通订单的最大可买可卖返回失败
     def test_acctradinginfo_query_us_normal_order_id(self):
         # 测试点：美股普通订单
         order_type = OrderType.NORMAL
@@ -152,72 +75,8 @@ class AcctradinginfoQuery(unittest.TestCase):
         print(ret_code)
         print(ret_data)
 
-    # A股
-    def test_acctradinginfo_query_cn_normal(self):
-        # 测试点：A股普通订单
-        order_type = OrderType.NORMAL
-        code = 'SZ.000034'
-        price = 5.7
-        order_id = None
-        adjust_limit = round(random.random(),3)
-        ret_code, ret_data = trade_ctx_cn.acctradinginfo_query(order_type, code, price, order_id,
-                                                                    adjust_limit=adjust_limit, trd_env=TrdEnv.SIMULATE,
-                                                                    acc_id=0, acc_index=0)
-        print(ret_code)
-        print(ret_data)
 
-    def test_acctradinginfo_query_cn_absolute_limit(self):
-        # 测试点：A股限价单
-        order_type = OrderType.ABSOLUTE_LIMIT
-        code = 'SH.601717'
-        price = 24
-        order_id = None
-        adjust_limit = random.random()
-        ret_code, ret_data = trade_ctx_cn.acctradinginfo_query(order_type, code, price, order_id,
-                                                                    adjust_limit=adjust_limit, trd_env=TrdEnv.SIMULATE,
-                                                                    acc_id=0, acc_index=0)
-        self.assertEqual(ret_code, RET_ERROR)
-        print(ret_data)
-
-    def test_acctradinginfo_query_cn_auction(self):
-        # 测试点：A股竞价单
-        order_type = OrderType.AUCTION
-        code = 'SH.600197'
-        price = 21
-        order_id = None
-        adjust_limit = random.random()
-        ret_code, ret_data = trade_ctx_cn.acctradinginfo_query(order_type, code, price, order_id,
-                                                                    adjust_limit=adjust_limit, trd_env=TrdEnv.SIMULATE,
-                                                                    acc_id=0, acc_index=0)
-        self.assertEqual(ret_code, RET_ERROR)
-        print(ret_data)
-
-    def test_acctradinginfo_query_cn_auction_limit(self):
-        # 测试点：A股竞价限价单
-        order_type = OrderType.AUCTION_LIMIT
-        code = 'SH.600987'
-        price = 11.5
-        order_id = None
-        adjust_limit = random.random()
-        ret_code, ret_data = trade_ctx_cn.acctradinginfo_query(order_type, code, price, order_id,
-                                                                    adjust_limit=adjust_limit, trd_env=TrdEnv.SIMULATE,
-                                                                    acc_id=0, acc_index=0)
-        self.assertEqual(ret_code, RET_ERROR)
-        print(ret_data)
-
-    def test_acctradinginfo_query_cn_special_limit(self):
-        # 测试点：A股特别限价单
-        order_type = OrderType.SPECIAL_LIMIT
-        code = 'SZ.002084'
-        price = 4.8
-        order_id = None
-        adjust_limit = random.random()
-        ret_code, ret_data = trade_ctx_cn.acctradinginfo_query(order_type, code, price, order_id,
-                                                                    adjust_limit=adjust_limit, trd_env=TrdEnv.SIMULATE,
-                                                                    acc_id=0, acc_index=0)
-        self.assertEqual(ret_code, RET_ERROR)
-        print(ret_data)
-
+    # bug单：下A股普通订单之后查询最大可买可卖数据有误
     def test_acctradinginfo_query_cn_normal_order_id(self):
         # 测试点：A股普通订单
         order_type = OrderType.NORMAL
@@ -243,31 +102,8 @@ class AcctradinginfoQuery(unittest.TestCase):
             self.assertTrue(ret_data[k][0] != '')
         print(ret_data)
 
-    # 入参错误用例
-    def test_acctradinginfo_query_err_code(self):
-        order_type = OrderType.NORMAL
-        code = 'hk.00700'
-        price = 0.1
-        order_id = None
-        adjust_limit = random.random()
-        ret_code, ret_data = trade_ctx_hk.acctradinginfo_query(order_type, code, price, order_id,
-                                                                    adjust_limit=adjust_limit, trd_env=TrdEnv.SIMULATE,
-                                                                    acc_id=0, acc_index=0)
-        self.assertEqual(ret_code, RET_ERROR)
-        print(ret_data)
 
-    def test_acctradinginfo_query_err_price1(self):
-        order_type = OrderType.NORMAL
-        code = 'HK.00700'
-        price = 0
-        order_id = None
-        adjust_limit = random.random()
-        ret_code, ret_data = trade_ctx_hk.acctradinginfo_query(order_type, code, price, order_id,
-                                                                    adjust_limit=adjust_limit, trd_env=TrdEnv.SIMULATE,
-                                                                    acc_id=0, acc_index=0)
-        self.assertEqual(ret_code, RET_ERROR)
-        print(ret_data)
-
+    # bug单：入参price==-1时acctradinginfo_query返回提示信息不明确
     def test_acctradinginfo_query_err_price2(self):
         order_type = OrderType.NORMAL
         code = 'HK.00700'
@@ -280,6 +116,7 @@ class AcctradinginfoQuery(unittest.TestCase):
         print(ret_code)
         print(ret_data)
 
+    # bug单：错误的order_id可以返回最大可买可卖
     def test_acctradinginfo_query_err_orderid(self):
         order_type = OrderType.NORMAL
         code = 'HK.00700'
@@ -291,31 +128,6 @@ class AcctradinginfoQuery(unittest.TestCase):
                                                                     acc_id=0, acc_index=0)
         # self.assertEqual(ret_code, RET_ERROR)
         print(ret_code)
-        print(ret_data)
-
-    def test_acctradinginfo_query_err_account(self):
-        order_type = OrderType.NORMAL
-        code = 'HK.00700'
-        price = 100
-        order_id = None
-        adjust_limit = random.random()
-        ret_code, ret_data = trade_ctx_hk.acctradinginfo_query(order_type, code, price, order_id,
-                                                               adjust_limit=adjust_limit, trd_env=TrdEnv.SIMULATE,
-                                                               acc_id=0, acc_index=10)
-        self.assertEqual(ret_code, RET_ERROR)
-        print(ret_code)
-        print(ret_data)
-
-    def test_acctradinginfo_query_idx(self):
-        order_type = OrderType.NORMAL
-        code = 'SH.000001'
-        price = 0.1
-        order_id = None
-        adjust_limit = random.random()
-        ret_code, ret_data = trade_ctx_cn.acctradinginfo_query(order_type, code, price, order_id,
-                                                               adjust_limit=adjust_limit, trd_env=TrdEnv.SIMULATE,
-                                                               acc_id=0, acc_index=0)
-        self.assertEqual(ret_code, RET_ERROR)
         print(ret_data)
 
 
